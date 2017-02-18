239208e83958a3eb1afbfbb597a03f287ceed437
Qian <pandachain731@gmail.com>
2017-02-18 10:20:19 -0800

    change format

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const jwtOptions = {};
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
 jwtOptions.secretOrKey = configure.secretKey;
 
-const strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   console.log('payload received', jwt_payload);
 
   User.findById(jwt_payload._id, function (err, user) {
@@ -66,13 +66,13 @@ passport.use(strategy);
 
 
 const checkRecipeOwnership = (req, res, next) => {
-  if(passport.authenticate('jwt', { session: false})){
-    Recipe.findById(req.body._id, function(err, foundRecipe){
-      if(err){
+  if (passport.authenticate('jwt', {session: false})) {
+    Recipe.findById(req.body._id, function (err, foundRecipe) {
+      if (err) {
         console.log(err);
         res.json(err)
-      }  else {
-        if(foundRecipe.author.id.equals(req.body.author.id)) {
+      } else {
+        if (foundRecipe.author.id.equals(req.body.author.id)) {
           next();
         } else {
           console.log(err);
@@ -92,7 +92,7 @@ app.get('/recipes', (req, res) => {
   })
 });
 
-app.post('/recipes', passport.authenticate('jwt', { session: false}), (req, res) => {
+app.post('/recipes', passport.authenticate('jwt', {session: false}), (req, res) => {
   Recipe.create(req.body, (err, recipe) => {
     if (err) console.log(err);
     else {
@@ -118,83 +118,83 @@ app.delete('/recipes', checkRecipeOwnership, (req, res) => {
     if (err) console.log(err);
     res.status(200)
   })
-  
+
 });
 
-app.post('/register', function(req, res, next) {
+app.post('/register', function (req, res, next) {
   const body = req.body;
   const hash = bcrypt.hashSync(body.password.trim(), 10);
   const user = new User({
     username: body.username.trim(),
     password: hash
   });
-  
-  user.save(function(err, user) { 
+
+  user.save(function (err, user) {
     if (err) {
       return res.status(401).json({
-      error: true,
-      message: 'Username has been used. Please try another one'
-    });
+        error: true,
+        message: 'Username has been used. Please try another one'
+      });
     }
-    
+
     const token = utility.generateToken(user);
 
     res.json({
       userInfo: {username: user.username, userid: user._id},
       token: token
-  });
+    });
   });
 });
 
-app.post('/users/login', function(req, res) {
+app.post('/users/login', function (req, res) {
   User
     .findOne({username: req.body.username})
-    .exec(function(err, user) {
+    .exec(function (err, user) {
       if (err) res.json(err);
       if (!user) {
         return res.status(401).json({
           error: true,
           message: 'Username or Password is Wrong'
-      });
+        });
       }
       bcrypt.compare(req.body.password, user.password,
-      function(err, valid) {
-        if (!valid) {
-          return res.status(401).json({
-            error: true,
-            message: 'Username or Password is Wrong'
+        function (err, valid) {
+          if (!valid) {
+            return res.status(401).json({
+              error: true,
+              message: 'Username or Password is Wrong'
+            });
+          }
+          const token = utility.generateToken(user);
+          res.json({
+            userInfo: {username: user.username, userid: user._id},
+            token: token
+          });
         });
-        }
-        const token = utility.generateToken(user);
-        res.json({
-          userInfo: {username: user.username, userid: user._id},
-          token: token
-      });
-      });
     });
 });
 
 //get current user from token
-app.get('/users/me', function(req, res, next) {
+app.get('/users/me', function (req, res, next) {
   // check header or url parameters or post parameters for token
   const token = req.query.token;
   if (!token) {
     return res.status(401).json({message: 'Must pass token'});
   }
 // Check token that was passed by decoding token using secret
-  jwt.verify(token, jwtOptions.secretOrKey, function(err, user) {
+  jwt.verify(token, jwtOptions.secretOrKey, function (err, user) {
     if (err) {
-      return next(err); 
+      return next(err);
     }
-    if(!user) {
+    if (!user) {
       return res.send("error")
     }
     //return user using the id from w/in JWTToken
-    User.findById(user._id, function(err, user) {
+    User.findById(user._id, function (err, user) {
       if (err) throw err;
       res.json({
         user: {username: user.username, userid: user._id}
-    });
+      });
     });
   });
 });
